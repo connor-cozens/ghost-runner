6d1def089e99dd49524836191c7ab3c35c194f3d
mbhatt26 <mbhatt26@uwo.ca>
2020-11-21 03:11:16 -0500

    updated profile

--- screens/ghost_tracker.tsx
+++ screens/ghost_tracker.tsx
@@ -7,9 +7,9 @@ import { Text, View } from '../components/Themed';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.js" />
+
+
+
     </View>
   );
 }
@@ -18,11 +18,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: '200',
+    fontStyle: 'italic',
+    justifyContent: 'space-between',
   },
   separator: {
     marginVertical: 30,
--- screens/profile.tsx
+++ screens/profile.tsx
@@ -10,13 +10,13 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>User Profile</Text>
       
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.title}>Username: </Text>
-      <Text style={styles.title}>Age: </Text>
-      <Text style={styles.title}>Best 2km Pace: </Text>
-      <Text style={styles.title}>Best 5km Pace: </Text>
-      <Text style={styles.title}>Best 10km Pace: </Text>
+      <Text style={styles.heading}>Username: mbhatt26</Text> 
+      <Text style={styles.heading}>Age: 22</Text>
+      <Text style={styles.heading}>Best 2km Pace: </Text> 
+      <Text style={styles.heading}>Best 5km Pace: </Text>
+      <Text style={styles.heading}>Best 10km Pace: </Text>
 
-      <EditScreenInfo path="/screens/profile.js" />
+
     </View>
   );
 }
@@ -25,11 +25,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: '200',
+    fontStyle: 'italic',
+    justifyContent: 'space-between',
   },
   separator: {
     marginVertical: 30,
