a917d3242546745b56a4fea678165e7291aad52a
Connor Cozens <cozcon@gmail.com>
2020-11-21 02:28:12 -0500

    Updated tabs & included map components.

--- screens/map.tsx
+++ screens/map.tsx
@@ -3,15 +3,30 @@ import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import { constructor } from 'react';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle} />
+        <MapView
+          initialRegion={{
+            latitude: 42.98491666389771,
+            longitude: -81.24528725322716,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.mapStyle} 
+        />
       </View>
     );
   }
+}
+
+
+const getMapDetails = () => {
+  // Get the current location of the user on the map
+
 }
 
 const styles = StyleSheet.create({
