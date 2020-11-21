7bc2a0c983540884767c42f20f0cf31e17ba53b1
Ryan Davis <rdavis54@uwo.ca>
2020-11-21 12:27:11 -0500

    adding debugging

--- api/app.js
+++ api/app.js
@@ -10,6 +10,8 @@ app.get('/get-user', function(req, res){
     MongoClient.connect("mongodb://127.0.0.1:27017", function(error, client){
         var db = client.db('ghost_db');
         db.collection('users').findOne({}, function (error, result){
+            console.log(error);
+            console.log(result);
             res.send(JSON.stringify(result));
         });
         
