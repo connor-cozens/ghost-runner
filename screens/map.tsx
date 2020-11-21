e969d32aa51e93f2b325c0e622842d1cbd7abe2e
Connor Cozens <cozcon@gmail.com>
2020-11-21 00:09:46 -0500

    Added map tab.

---
+++ screens/map.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
