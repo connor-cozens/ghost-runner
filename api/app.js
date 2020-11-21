74b275c1d5393ae36f8776e4de2ae10aaf885603
Ryan Davis <rdavis54@uwo.ca>
2020-11-21 12:24:22 -0500

    fixed error

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
 })
 
 app.get('/get-user', function(req, res){
-    MongoClient.connect("mongodb://127.0.0.1:27017", function(error, db){
+    MongoClient.connect("mongodb://127.0.0.1:27017", function(error, client){
         var db = client.db('ghost_db');
         db.collection('users').findOne({}, function (error, result){
             res.send(JSON.stringify(result));
