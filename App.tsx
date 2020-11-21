037ed1d1e8d193857031a655280dc1b23aab1838
Ryan Davis <rdavis54@uwo.ca>
2020-11-21 17:26:05 -0500

    Zooms into current location on startup

--- App.tsx
+++ App.tsx
@@ -4,11 +4,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, StackScreenProps } from '@react-navigation/stack';
 import Animated from 'react-native-reanimated';
 
+import * as Location from 'expo-location'
+
 // Import Individual pages as Objects
 import MapScreen from './screens/Map';
 import GhostPickerScreen from './screens/Ghost_Picker';
 import LoginScreen from './screens/Login';
 import ProfileScreen from './screens/Profile';
+import { Alert } from 'react-native';
 // import LandingScreen from './screens/Landing_Page';
 
 // Stack Navigation Control
@@ -22,9 +25,16 @@ export default class App extends Component {
     this.state = {
       sliding_animation: new Animated.Value(0),
     }
+    this.enableLocation();
   }
 
-  //
+  async enableLocation() {
+    let { status } = await Location.requestPermissionsAsync()
+    if (status !== 'granted'){
+      Alert.alert('Please Allow Location')
+    }
+  }
+  //s
   render() {
     return (
       <NavigationContainer>
--- screens/Map/index.js
+++ screens/Map/index.js
@@ -2,14 +2,37 @@ import * as React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
 
-export default function MapScreen ({ navigation }) {
-  // render() {
+import * as Location from 'expo-location'
+
+export default class MapScreen extends React.Component {
+
+  constructor({ navigation }){
+    super()
+    this.navigation = navigation;
+
+    this.state = {
+      lat: -49.98491666389771,
+      long: -81.24528725322716,
+    }
+  }
+
+  componentDidMount(){
+    Location.getCurrentPositionAsync()
+    .then((location) => {
+      this.setState({
+        lat: location["coords"]["latitude"],
+        long: location["coords"]["longitude"]
+      });
+    });
+  }
+
+  render() {
     return (
       <View style={styles.container}>
       <MapView
-        initialRegion={{
-          latitude: 42.98491666389771,
-          longitude: -81.24528725322716,
+        region={{
+          latitude: this.state.lat,
+          longitude: this.state.long,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
@@ -18,27 +41,27 @@ export default function MapScreen ({ navigation }) {
       {/* <View style = {styles.container}> */}
         
         <Text 
-          onPress = { () => navigation.navigate('Ghost')} 
+          onPress = { () => this.navigation.navigate('Ghost')} 
           style={styles.ghost}
           >Ghost
         </Text>
         
         <Text 
-          onPress = { () => navigation.navigate('Login')} 
+          onPress = { () => this.navigation.navigate('Login')} 
           style={styles.login}
           >Login
         </Text>
         
         <Text 
-          onPress = { () => navigation.navigate('Profile')} 
+          onPress = { () => this.navigation.navigate('Profile')} 
           style={styles.profile}
           >Profile
         </Text>
       
       {/* </View> */}
     </View>
-    )
-  // }  
+    );
+  }  
 }
 
 const styles = StyleSheet.create({
