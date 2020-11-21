0c09d33e441d83f006a404593078896cf2ed2854
Ryan Davis <rdavis54@uwo.ca>
2020-11-21 13:05:04 -0500

    changed order of code

--- api/app.js
+++ api/app.js
@@ -2,14 +2,16 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 
 var bodyParser = require('body-parser')
+
+
+const app = express()
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
 app.use(express.json())
-
-const app = express()
 
 app.get('/', function(req, res){
     res.send('some text')
