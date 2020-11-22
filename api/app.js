d5d5db2d13d0e767d010dd36cee72a732047b6ed
Ryan Davis <rdavis54@uwo.ca>
2020-11-22 04:22:19 -0500

    fixing app.js

--- api/app.js
+++ api/app.js
@@ -41,7 +41,7 @@ app.get('/get-user', function(req, res){
 
 app.post('/create-user', function(req, res){
     MongoClient.connect("mongodb://127.0.0.1:27017", function(error, client){
-        var db = client.db('ghost_db');
+        var db = client.db('ghost-db')
         db.collection('users').insert({username: req.username, password: req.username, age: req.age}, function(error, result){
             res.send(JSON.stringify(result))
         })
@@ -52,23 +52,32 @@ app.post('/verify-user', function(req, res){
     var data = req.body
     console.log(data);
     MongoClient.connect("mongodb://127.0.0.1:27017", function(error, client){
-        var db = client.db('ghost_db');
-        db.collection('users').findOne({username: req.username, password: req.password}, function(error, result){
-            res.send(JSON.stringify(result))
+        var db = client.db('ghost_db')
+        db.collection('users').findOne({username: data.username, password: data.password}, {projection: {_id: 1}}, function(error, result){
+            if (result != null) {
+                result["signedIn"] = true;
+                console.log(result);
+            } else {
+                console.log('User Not Found')
+            }
+            res.send(JSON.stringify(result));
         })
     })
 })
 
-app.get('/get-users', function(req, res){
+app.get('/test', function (req, res){
+    console.log("HIT")
+    var hi = {test : "testmessage"};
+    res.send(JSON.stringify(result));
+})
+
+app.post('/get-ghost-run', function(req, res){
+    var data = req.body
+    console.log(data)
+    var oid = new ObjectID(data.oid)
+    console.log(oid);
     MongoClient.connect("mongodb://127.0.0.1:27017", function(error, client){
         var db = client.db('ghost_db');
-<<<<<<< HEAD
-        db.collection('users').find({}, function(error, result){
-            res.send(JSON.stringify(result))
-        })
-    })
-})
-=======
         db.collection('users').findOne({_id: oid}, {projection: {latest_run: 1}}, function (error, result){
             res.send(JSON.stringify(result));
         });
@@ -129,16 +138,17 @@ app.post('/upload-run', function(req, res){
 // app.listen(8080, () => {
 //     console.log('app starting')
 // })
->>>>>>> fb55591d120fedfe931eb04625d47eb0d23c47a0
-
-app.get('/home', (req, res) => {
-    res.send('we are on home')
-})
 
 
-https.createServer({
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem'),
-    passphrase: 'letmein'
-}, app)
-app.listen(8080);
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("/etc/letsencrypt/live/ghost.ryandavis.tech/privkey.pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/live/ghost.ryandavis.tech/fullchain.pem"),
+    },
+    app
+  )
+  .listen(8080, () => {
+    console.log('Listening...')
+  })
